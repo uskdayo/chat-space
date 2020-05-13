bfa346746dd268f9a4211d963362b55345c56984
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-05-13 15:57:01 +0900

    setting s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'katochatspace'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/katochatspacevim ~/.zshrc'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/katochatspace'
 end

6e5ee232584676b7a002647383910cbd1a133914
uskdayo <64318733+uskdayo@users.noreply.github.com>
2020-05-15 21:35:01 +0900

    fix4

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,6 @@
 $(function() {
+
+  var name=[];
   function addUser(user) {
     let html = `
       <div class="chat-group-user clearfix">
@@ -18,30 +20,41 @@ $(function() {
     $("#user-search-result").append(html);
   }
   function addDeleteUser(name, id) {
-    let html = `
-    <div class="chat-group-user clearfix" id="${id}">
-      <p class="chat-group-user__name">${name}</p>
-      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
-    </div>`;
+    let html = `<div class="chat-group-user clearfix js-chat-member">
+                <input name="group[user_ids][]" type="hidden" value="${id}">
+                <p class="chat-group-user__name">
+                ${name}
+                </p>
+                <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">
+                削除
+                </a>
+                </div>`
     $(".js-add-user").append(html);
   }
-  function addMember(userId) {
-    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
-    $(`#${userId}`).append(html);
-  }
+  // function addMember(userId) {
+  //   let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" >`;
+  //   $(`#${userId}`).append(html);
+  // }
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     let member = $("#chat-group-users").find("p").text;
-    console.log(member);
+    // let exceptId = []
+    let members = $('input[name="group[user_ids][]"]').map(function(){
+                       return $(this).val();
+                    });
+    let exceptIds = members.get();
 
     $.ajax({
       type: "GET",
       url: "/users",
-      data: { keyword: input },
+      data: { keyword: input,
+              ids: exceptIds
+              },
       dataType: "json"
     })
       .done(function(users) {
         $("#user-search-result").empty();
+
         if (users.length !== 0) {
           users.forEach(function(user) {
             addUser(user);
@@ -57,18 +70,24 @@ $(function() {
       });
   });
   $(document).on("click", ".chat-group-user__btn--add", function() {
-    console.log
+    // console.log
     const userName = $(this).attr("data-user-name");
     const userId = $(this).attr("data-user-id");
+    name.push(userName);
+    // console.log(name);
+   
     $(this)
       .parent()
       .remove();
     addDeleteUser(userName, userId);
-    addMember(userId);
+    // addMember(userId);
+
   });
   $(document).on("click", ".chat-group-user__btn--remove", function() {
     $(this)
       .parent()
       .remove();
+      const userName = $(this).attr("data-user-name");
+      name.pop(userName);
   });
 });
